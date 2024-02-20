b6c32e145d3ab127b415c2513b7a1612502dd5aa
Jens Krieger <jens@jk13.dev>
2024-02-20 18:53:55 +0100

    Updated paths

--- latest/misc/cronjob_backup.sh
+++ latest/misc/cronjob_backup.sh
@@ -8,9 +8,9 @@ add_cron_job() {
         exit 1
     fi
 
-    echo "0 5 * * * curl --silent https://localhost/backup/adif/$key &>/dev/null" >> /etc/crontab
-    echo "10 5 * * * curl --silent https://localhost/backup/notes/$key &>/dev/null" >> /etc/crontab
-    echo "20 5 * * * root find /var/www/html/backup -name 'logbook_*' -o -name 'notes_*' -type f -printf '%T@ %p\n' | sort -k1,1nr | awk 'NR>30 {print $2}' | xargs rm -f" >> /etc/crontab
+    echo "0 5 * * * curl --silent https://localhost/backup/adif/$key &>/dev/null" >> /var/www/html/crontab/crontab
+    echo "10 5 * * * curl --silent https://localhost/backup/notes/$key &>/dev/null" >> /var/www/html/crontab/crontab
+    echo "20 5 * * * root find /var/www/html/backup -name 'logbook_*' -o -name 'notes_*' -type f -printf '%T@ %p\n' | sort -k1,1nr | awk 'NR>30 {print $2}' | xargs rm -f" >> /var/www/html/crontab/crontab
     echo "Cronjobs added successfully."
 }
 
